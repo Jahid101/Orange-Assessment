1adc289dd9eacc5887eb5071047711ac239d6f5c
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-09-11 15:44:40 +0600

    index page

---
+++ Components/MyProfile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+require('./index.less')
+
+const index = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Dashboard from "@Components/Dashboard";
+import AppLayout from "@layout/layout";
 import type { NextPage } from "next";
 // import '../styles/globals.less';
 require("../styles/globals.less");
@@ -5,6 +7,7 @@ require("../styles/globals.less");
 const Home: NextPage = () => {
   return (
     <AppLayout>
+      <Dashboard></Dashboard>
     </AppLayout>
   );
 };
