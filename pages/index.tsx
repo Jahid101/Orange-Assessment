b1022a92127f05ddbd74c14257ba0e958807be2c
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-09-11 15:33:54 +0600

    globals

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import '../styles/globals.less';
-// require('../styles/globals.less');
+// import '../styles/globals.less';
+require('../styles/globals.less');
 
 const Home: NextPage = () => {
   return <div className="primary-color">Hi</div>;
