2cf94d656665eecfa380d6cc2f3ecb1cc0f12649
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-09-25 21:28:23 +0600

    api connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,11 @@ import { useState } from "react";
 require("./index.less");
 
 const index = () => {
-<<<<<<< HEAD
-  let arr: any = [1,2,3,4];
-  let abc = arr.map((item: any) => +item % 2 == 0);
-  console.log(abc);
-=======
   const [visible, setVisible] = useState(false);
 
->>>>>>> 83de0fd52ded8d38ed944e5bbe5ada7ab6e20811
+  let arr: any = [1, 2, 3, 4];
+  let abc = arr.map((item: any) => +item % 2 == 0);
+  console.log(abc);
   return (
     <AppLayout>
       <Layout className="not-collapsed">
@@ -40,24 +37,16 @@ const index = () => {
                 defaultSelectedKeys={"1"}
               ></MobileDrawer>
 
-<<<<<<< HEAD
               <Col span={9} className="text-right">
-=======
-              <Col span={21} className="text-right">
->>>>>>> 83de0fd52ded8d38ed944e5bbe5ada7ab6e20811
                 <MyProfile></MyProfile>
               </Col>
             </Row>
           </Header>
 
           <Content
-<<<<<<< HEAD
             className="uniPadding"
             // style={{ margin: "10px", minHeight: "100vh" }}
           >
-=======
-            className="uniPadding" >
->>>>>>> 83de0fd52ded8d38ed944e5bbe5ada7ab6e20811
             <div className="name capitalize mb-30">Dashboard</div>
             <div className="mt-50">
               <Empty />
